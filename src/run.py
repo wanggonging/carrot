d3a0e5c7cfc6a31898e0aa481e4e5c85590510a8
Wang Gonging <wanggonging@gmail.com>
2018-11-03 12:37:55 +0800

    index parsing

--- src/run.py
+++ src/run.py
@@ -1,6 +1,8 @@
+import io
 import json
 import os
 import pprint
+import time
 import urllib2
 from bs4 import BeautifulSoup
 
@@ -28,7 +30,35 @@ def main():
             print("Saved to cache file")
 
         soup = BeautifulSoup(html, "lxml")
-        pprint.pprint(soup.title)
+        i = 0
+        now = int(time.time())
+
+        channel_index_file = channel["id"]+".json"
+        if os.path.exists(channel_index_file):
+            with open(channel_index_file) as f:
+                index = json.load(f)
+            print(len(index), "items loaded")
+        else:
+            index = {}
+        for h3 in soup.find_all("h3", class_="yt-lockup-title"):
+            href=unicode(h3.a['href'])
+            if (len(href) == 20) and (href[0:9] == "/watch?v="):
+                key=href[9:20]
+                if not key in index:
+                    title=h3.a.get_text()
+                    published=now-i
+                    index[key] = {'key':key, 'published':published, 'title':title}
+                    print("Added: " + title)
+                #pprint.pprint(index[key])
+                i = i+1
+
+        print(len(index), " items after parsing new html")
+        # dump to file
+        #for item in sorted(index.values(), key=lambda x: x['published']):
+        #    pprint.pprint(item)
+
+        with io.open(channel["id"]+".json", "w") as f:
+            f.write(json.dumps(index, ensure_ascii=False))
 
 
 main()
